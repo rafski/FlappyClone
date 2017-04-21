1417bb1963cd033062c3ab79090534e2711f69aa
Raf <rkrassowski@gmail.com>
2017-04-21 09:59:10 +0100

    Initial commit

---
+++ html/src/com/rafski/flappyclone/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.rafski.flappyclone.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.rafski.flappyclone.FlappyClone;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new FlappyClone();
+        }
+}
